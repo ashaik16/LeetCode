9d5506c73e359689a6c0f0d077b50fa6411d015e
ashaik16 <71361552+ashaik16@users.noreply.github.com>
2021-11-28 01:21:26 -0600

    https://leetcode.com/problems/remove-duplicates-from-sorted-array/submissions/

---
+++ RemoveDuplicate.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+ List<Integer>list=new ArrayList<Integer>();
+             
+for(int i=0;i<nums.length;i++){
+ if(!list.contains(nums[i]))
+ list.add(nums[i]);
+    
+}
+for(int i=0;i<list.size();i++)
+    nums[i]=list.get(i);
+  
+    
+ return list.size(); 
+}
+}
