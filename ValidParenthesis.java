640db599b98885c5f404f799f795deaf3c06bf42
ashaik16 <71361552+ashaik16@users.noreply.github.com>
2021-12-05 16:59:15 -0600

    Create ValidParenthesis.java
    https://leetcode.com/problems/valid-parentheses/

---
+++ ValidParenthesis.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isValid(String s) {
+		char c1[] = { '(', '{', '[' };
+		char c2[] = { ')', '}', ']' };
+		int newFound = 0;
+		Stack<Character> stack = new Stack<>();
+
+		System.out.println("Length" + s.length());
+    if(s.length()==1)
+        return false;
+		for (int i = 0; i < s.length() ; i++) {
+			int found = new String(c1).indexOf(s.charAt(i));
+			if(found==-1)
+			{
+				 newFound = new String(c2).indexOf(s.charAt(i));
+			if(!stack.isEmpty() && c1[newFound]!=stack.peek())
+			return false;
+			else if(stack.isEmpty())
+				return false;
+						
+			else
+				stack.pop();
+			}
+			else
+				stack.push(s.charAt(i));
+
+	}
+        	if(!stack.isEmpty())
+			return false;
+		return true;
+	}
+     
+    }
+
